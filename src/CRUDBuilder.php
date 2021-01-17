7b04326c0b47d829a504757319b59e633d0eb218
muhamadhhassan <muhamadhhassan@gmail.com>
2021-01-17 22:49:06 +0200

    feat: add CRUD builder class, request validation trait and synced relation trait

---
+++ src/CRUDBuilder.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace CrudBuilder;
+
+use CrudBuilder\Traits\RequestValidator;
+use CrudBuilder\Traits\SyncedRelation;
+
+class CRUDBuilder
+{
+    use RequestValidator, SyncedRelation;
+
+    /**
+     * The resource namespace form example App\Models\User.
+     *
+     * @var string
+     */
+    public $resource;
+
+    /**
+     * The singular name of the resource for example user.
+     *
+     * @var string
+     */
+    public $resourceName;
+
+    /**
+     * The plural name of the resource for example users.
+     *
+     * @var string
+     */
+    public $resourceNamePlural;
+
+    /**
+     * The route of the resource.
+     *
+     * @var string
+     */
+    public $route;
+
+    /**
+     * The route of the resource.
+     *
+     * @var Illuminate\Http\Request
+     */
+    public $request;
+
+    /**
+     * The field that make the resource recognizable like name, title or full_name.
+     *
+     * @var string
+     */
+    public $recognizedBy = 'name';
+
+    /**
+     * The inputs in the creation form.
+     *
+     * @var array
+     */
+    public $createInputs = [];
+
+    /**
+     * The inputs in the edit form.
+     *
+     * @var array
+     */
+    public $updateInputs = [];
+
+    /**
+     * The request used to validate resource creation.
+     *
+     * @var string
+     */
+    public $createRequest;
+
+    /**
+     * The request used to validate resource creation.
+     *
+     * @var string
+     */
+    public $updateRequest;
+
+    /**
+     * The columns that will be in the index page.
+     *
+     * @var array
+     */
+    public $indexColumns = [];
+
+    /**
+     * The allowed actions on the CRUD.
+     *
+     * @var array
+     */
+    public $actions = ['index', 'create', 'edit', 'delete', 'show'];
+
+    /**
+     * Sets the namespace of the CRUD resource.
+     *
+     * @param string $resourceNamespace
+     * @return \CRUDBuilder
+     */
+    public function setResource(string $resourceNamespace)
+    {
+        if (! class_exists($resourceNamespace)) {
+            throw new \Exception("The model '{$resourceNamespace}' does not exist.", 500);
+        }
+
+        if (! is_subclass_of((new $resourceNamespace()), 'Illuminate\Database\Eloquent\Model')) {
+            throw new \Exception("The class '{$resourceNamespace}' must be an instance of 'Illuminate\Database\Eloquent\Model'", 500);
+        }
+
+        $this->resource = $resourceNamespace;
+
+        return $this;
+    }
+
+    /**
+     * Sets the singular and plural names of the CRUD resource.
+     *
+     * @param string $single
+     * @param string $plural
+     * @return \CRUDBuilder
+     */
+    public function setResourceNames(string $single, string $plural)
+    {
+        $this->resourceName = $single;
+        $this->resourceNamePlural = $plural;
+
+        return $this;
+    }
+
+    /**
+     * Sets the route to the CRUD resource.
+     *
+     * @param string $route
+     * @return \CRUDBuilder
+     */
+    public function setRoute(string $route)
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    /**
+     * Sets the route to the CRUD resource using named routes.
+     *
+     * @param string $route
+     * @param array $params
+     * @return \CRUDBuilder
+     */
+    public function setRouteName(string $route, array $params = [])
+    {
+        $complete_route = $route.'.index';
+
+        if (! \Route::has($complete_route)) {
+            throw new \Exception('There are no routes for this route name.', 404);
+        }
+
+        $this->route = route($complete_route, $params);
+
+        return $this;
+    }
+
+    /**
+     * Set the allowed actions on the CRUD.
+     *
+     * @param array $actions
+     * @return \CRUDBuilder
+     */
+    public function setActions(array $actions)
+    {
+        $this->actions = $actions;
+
+        return $this;
+    }
+
+    /**
+     * Returns the resource route.
+     *
+     * @return string
+     */
+    public function getRouteName(): string
+    {
+        return $this->route;
+    }
+
+    /**
+     * Add an input to the create inputs collection.
+     *
+     * @param mixed $inputs
+     * @return \CRUDBuilder
+     */
+    public function addCreateInputs($inputs)
+    {
+        if (! is_array($inputs)) {
+            array_push($this->createInputs, $inputs);
+
+            return $this;
+        }
+
+        foreach ($inputs as $input) {
+            if ($input instanceof \CrudBuilder\Helpers\Forms\Input) {
+                array_push($this->createInputs, $input);
+            } else {
+                throw new \Exception('Form inputs must be an instance of App\Helpers\CRUD\Forms\Input.php.', 500);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add an input to the update inputs collection.
+     *
+     * @param mixed $inputs
+     * @return \CRUDBuilder
+     */
+    public function addUpdateInputs($inputs)
+    {
+        if (! is_array($inputs)) {
+            array_push($this->updateInputs, $inputs);
+
+            return $this;
+        }
+
+        foreach ($inputs as $input) {
+            if ($input instanceof \CrudBuilder\Helpers\Forms\Input) {
+                array_push($this->updateInputs, $input);
+            } else {
+                throw new \Exception('Form inputs must be an instance of App\Helpers\CRUD\Forms\Input.php.', 500);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add a column to the index columns collection.
+     *
+     * @param mixed $columns
+     * @return \CRUDBuilder
+     */
+    public function addIndexColumns($columns)
+    {
+        if (! is_array($columns)) {
+            array_push($this->indexColumns, $columns);
+
+            return $this;
+        }
+
+        foreach ($columns as $column) {
+            if ($column instanceof \CrudBuilder\Helpers\IndexColumn) {
+                array_push($this->indexColumns, $column);
+            } else {
+                throw new \Exception('Index table columns must be an instance of App\Helpers\CRUD\IndexColumn.php.', 500);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Determine if an action is permitted.
+     *
+     * @param string $action
+     * @return bool
+     */
+    public function can(string $action) : bool
+    {
+        if (! in_array($action, $this->actions)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine if an action is permitted. Abort if not.
+     *
+     * @param string|[string] $action
+     *
+     * @return bool|null
+     */
+    public function canOrFail($action) : ?bool
+    {
+        if (! in_array($action, $this->actions)) {
+            abort(403, 'Unauthorized Access');
+        }
+
+        return true;
+    }
+}
---
+++ src/Traits/SyncedRelation.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace CrudBuilder\Traits;
+
+use CrudBuilder\Helpers\SyncedEntity;
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+
+trait SyncedRelation
+{
+    /**
+     * The many to many relations that will require syncing.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    public $syncedEntities = [];
+
+    /**
+     * Add a relation to the synced relation collection.
+     *
+     * @param string $className
+     * @param array $properties
+     * @param array $pivotProperties
+     * @param string $relation
+     * @return \CRUDBuilder
+     */
+    public function addSyncedEntity(string $className, array $properties, array $pivotProperties, string $relation, bool $taggable = false)
+    {
+        if (! class_exists($className)) {
+            throw new \Exception("The model '{$className}' does not exist.", 500);
+        }
+
+        if (! is_subclass_of((new $className()), 'Illuminate\Database\Eloquent\Model')) {
+            throw new \Exception("The class '{$className}' must be an instance of 'Illuminate\Database\Eloquent\Model'", 500);
+        }
+
+        array_push($this->syncedEntities, new SyncedEntity($className, $properties, $pivotProperties, $relation, $taggable));
+
+        return $this;
+    }
+
+    /**
+     * Return an array of relations' names.
+     *
+     * @return array
+     */
+    public function getSyncedRelations()
+    {
+        return collect($this->syncedEntities)
+            ->map(function ($entity) {
+                return $entity->relation;
+            })->toArray();
+    }
+
+    public function attach(Model $resource, array $attachments)
+    {
+        foreach ($attachments as $relation => $attachment) {
+            $ids = $this->getIds($relation, $attachment);
+            $resource->$relation()->attach($ids);
+        }
+    }
+
+    public function sync(Model $resource, array $attachments)
+    {
+        foreach ($attachments as $relation => $attachment) {
+            $ids = $this->getIds($relation, $attachment);
+            $resource->$relation()->sync($ids);
+        }
+    }
+
+    /**
+     * Return the entity ids to be attached or synced to the resource.
+     *
+     * @param string $relation
+     * @param array $inputIds
+     * @return array
+     */
+    protected function getIds(string $relation, array $inputIds): array
+    {
+        $syncedEntity = collect($this->syncedEntities)->filter(function ($entity) use ($relation) {
+            return $entity->relation === $relation;
+        })->first();
+
+        $filteredIds = collect($inputIds)->map(function ($id) use ($syncedEntity) {
+            if (! $syncedEntity->className::find($id) && $syncedEntity->createNewRecord) {
+                return $syncedEntity->className::create(['name' => $id])->id;
+            }
+
+            return $id;
+        })->toArray();
+
+        return $filteredIds;
+    }
+
+    public function getParentProperty(Model $entity, string $relation, $property)
+    {
+        $parent = collect($this->syncedEntities)
+            ->filter(function ($entity) use ($relation) {
+                return $entity->relation === $relation;
+            })->first();
+
+        if (! $parent) {
+            throw new Exception("No parent entities with the relation '$property'", 500);
+        }
+
+        return $entity->$relation->$property;
+    }
+}
