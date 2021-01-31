978f85ab42a355de760dd2933e9fadd33cb94d3e
muhamadhhassan <muhamadhhassan@gmail.com>
2021-01-31 13:34:18 +0200

    feat: change the text editor to quilljs

--- src/resources/views/components/inputs/text-editor.blade.php
+++ src/resources/views/components/inputs/text-editor.blade.php
@@ -2,12 +2,12 @@
   @if($attributes['label'])
     <x-crudbuilder::utils.input-label :label="$attributes['label']" :name="$name" :mandatory="$mandatory"/>
   @endif
-  <textarea 
-    id="{{ $attributes['id'] ?? Str::random(5) }}"
-    class="{{ 'summernote ' . $attributes['class'] }}"
-    name="{{ $name }}">
+  <div 
+    id="{{ $attributes['id'] }}"
+    class="{{ $attributes['class'] }}">
     {!! $value !!}
-  </textarea>
+  </div>
+  <input type="hidden" name="{{ $name }}" id="{{ $attributes['id'].'value' }}">
   @isset($error)
     <div class="invalid-feedback">{{ $error }}</div>
   @endisset
@@ -15,3 +15,38 @@
     <span class="form-text text-muted">{{ $helpText }}</span>
   @endisset
 </div>
+
+@prepend(config('crudbuilder.layouts.stacks.styles'))
+  <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
+@endprepend
+
+@prepend(config('crudbuilder.layouts.stacks.scripts'))
+  <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      var quill = new Quill('{{ '#' . $attributes['id'] }}', {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike', 'image'],        // toggled buttons
+            ['blockquote', 'code-block'],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+            ['clean']                                         // remove formatting button
+          ]
+        },
+        theme: 'snow'
+      });
+
+      $("{{'#'.$attributes['id'].'value'}}").val(quill.container.firstChild.innerHTML);
+
+      quill.on('text-change', function(delta, oldDelta, source) {
+        $("{{'#'.$attributes['id'].'value'}}").val(quill.container.firstChild.innerHTML);
+      });
+    });
+  </script>
+@endprepend
