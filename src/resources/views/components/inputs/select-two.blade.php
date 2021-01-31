1c3eb158cfcaf830ef9fb13ffbee05034dd5485a
muhamadhhassan <muhamadhhassan@gmail.com>
2021-01-31 13:30:01 +0200

    fix: remove styles link from scripts

--- src/resources/views/components/inputs/select-two.blade.php
+++ src/resources/views/components/inputs/select-two.blade.php
@@ -28,7 +28,6 @@
 @endprepend
 
 @prepend(config('crudbuilder.layouts.stacks.scripts'))
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
