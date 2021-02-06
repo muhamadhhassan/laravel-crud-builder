c5d734b98701fa080683c34e41e29da7148a484d
muhamadhhassan <muhamadhhassan@gmail.com>
2021-02-06 13:40:25 +0200

    fix: hide password values

--- src/Views/Components/Inputs/Text.php
+++ src/Views/Components/Inputs/Text.php
@@ -14,20 +14,28 @@ class Text extends InputComponent
     public $value;
 
     /**
+     * The text type of the input
+     *
+     * @var string
+     */
+    public $type;
+
+    /**
      * Create a new component instance.
      *
      * @param string $name
      * @param bool $mandatory
      * @param \Illuminate\Database\Eloquent\Model $resource
      */
-    public function __construct(string $name, bool $mandatory = true, Model $resource = null)
+    public function __construct(string $name, bool $mandatory = true, Model $resource = null, string $type = 'text')
     {
+        $this->type = $type;
         parent::__construct($name, $mandatory, $resource);
     }
 
     protected function setPredefinedValue()
     {
-        if ($this->resource) {
+        if ($this->resource && $this->type !== 'password') {
             $propertyName = $this->name;
             $this->value = $this->resource->$propertyName;
         }
